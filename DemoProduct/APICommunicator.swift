dba77cf15a0ea796dca776c70107175a52fa659a
spiderzys <spiderzys@hotmail.com>
2016-08-11 23:49:24 -0400

    UI

---
+++ DemoProduct/APICommunicator.swift
@@ -0,0 +1,13 @@
+//
+//  APICommunicator.swift
+//  DemoProduct
+//
+//  Created by YANGSHENG ZOU on 2016-08-11.
+//  Copyright © 2016 YANGSHENG ZOU. All rights reserved.
+//
+
+import UIKit
+
+class APICommunicator: NSObject {
+
+}
